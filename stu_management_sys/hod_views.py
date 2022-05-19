a6936319c7ef244e03a56c0bf4bafebe061f2d84
you <97931972+smsamrat@users.noreply.github.com>
2022-05-19 12:21:02 +0600

    login required

--- stu_management_sys/hod_views.py
+++ stu_management_sys/hod_views.py
@@ -1,4 +1,7 @@
+import imp
 from django.shortcuts import render,redirect, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/')
 def HOME(request):
     return render(request,'hod/home.html')
