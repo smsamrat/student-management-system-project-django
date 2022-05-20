bbe0dc79d018d0a55423ed050311df6fcb70d044
you <97931972+smsamrat@users.noreply.github.com>
2022-05-20 20:28:10 +0600

    Add student functionality from form to database

--- stu_management_sys/hod_views.py
+++ stu_management_sys/hod_views.py
@@ -1,6 +1,7 @@
+from django.contrib import messages
 from django.shortcuts import render,redirect, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from app.models import CustomUser,Course,SessionYear
+from app.models import CustomUser,Course,SessionYear,Student
 
 @login_required(login_url='/')
 def HOME(request):
@@ -10,7 +11,53 @@ def HOME(request):
 def addStudent(request):
     course = Course.objects.all()
     session_year = SessionYear.objects.all()
-    print(session_year)
+
+    if request.method == "POST":
+        profile_pic = request.FILES.get('profile_pic')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+        gender = request.POST.get('gender')
+        course_id = request.POST.get('course_id')
+        session_year_id = request.POST.get('session_year_id')
+
+        session_year_id = request.POST.get('session_year_id')
+        if CustomUser.objects.filter(email=email).exists():
+            messages.warning(request,'Email Already Exist')
+            return redirect('add_student')
+
+        if CustomUser.objects.filter(username=username).exists():
+            messages.warning(request,'Username Already Exist')
+            return redirect('add_student')
+        else:
+            user =CustomUser(
+                first_name = first_name,
+                last_name = last_name,
+                username = username,
+                email = email,
+                profile_pic = profile_pic,
+                user_type = 2
+            )
+            user.set_password(password)
+            user.save()
+
+            course = Course.objects.get(id = course_id)
+            session_year = SessionYear.objects.get(id = session_year_id)
+
+            student = Student(
+                admin = user,
+                address = address,
+                session_year_id = session_year,
+                course_id = course,
+                gender = gender,
+            )
+            student.save()
+            messages.success(request, user.first_name + "  " + user.last_name + " Are Successfully Added !")
+            return redirect('add_student')
+
     context ={
         'course':course,
         'session_year':session_year,
